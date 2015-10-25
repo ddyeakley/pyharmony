54ab92f9c1c152ac31c8cd1dcce88070df4ff0f0
e412646 <Daniel.Yeakley@Honeywell.com>
2015-10-25 13:49:32 -0400

    Added setup.py  for pip install
    Change-Id: I6540c404974723f2c0567db1cea6db8b7b5395ec

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup
+
+setup(
+    name='pyharmony',
+    version='0.1',
+    license='See LICENSE',
+    author='Jeff Terrace',
+    author_email='Jeff Terrace <jterrace@gmail.com>',
+    url='https://github.com/ddyeakley/pyhwtherm',
+    long_description="README.md",
+    packages=['pyharmony'],
+    include_package_data=False,
+    description="Python library for connecting to and controlling the Logitech Harmony Link",
+)
